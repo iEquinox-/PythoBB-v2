beccad9907294c04c36884e8cc6a48d1769f57d1
Grayson Equinox <duubz@skid.pro>
2015-03-03 02:18:54 -0600

    Update Frame.py

--- Frame.py
+++ Frame.py
@@ -1,14 +1,11 @@
-import Security,Render,Pages
+import Security,Render,Pages,Database
 
 class Base:
 	def Home(self, request):
 		if request.COOKIES.has_key("sid"):
-			user_status = True
-			sid = request.COOKIES["sid"]
+			user_status,sid = True,request.COOKIES["sid"]
 		else:
-			user_status = False
-			sid = None
-			
+			user_status,sid = False,None
 		return Render.Render()._Page(content=Pages.Pages()._FullRender(
 				content=Pages.Pages()._Render(name=Pages.Pages().pageKeys["index"]),
 				condit = {
