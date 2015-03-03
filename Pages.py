55302f329855a7d3540bf27826c0422c8f6080dc
Grayson Equinox <duubz@skid.pro>
2015-03-02 20:58:08 -0600

    Update Pages.py

--- Pages.py
+++ Pages.py
@@ -1,7 +1,9 @@
+import os,Render
 
 class Pages():
 	def __init__(self):
 		self._Render = self.OpenPage
+		self._CSS    = self.RenderCSS
 		self.pageKeys = {
 			"index": "index",
 			"userblock_guest": "",
@@ -9,4 +11,7 @@ class Pages():
 			}
 		
 	def OpenPage(self, name=None):
-		return str(open("templates/%s.ptmp"%(name)).read())
+		return str(open(os.path.dirname(os.path.abspath(__file__))+"/templates/%s.ptmp"%(name)).read())
+
+	def RenderCSS(self, request, fname):
+		return Render.Render()._Page(content=str(open(os.path.dirname(os.path.abspath(__file__))+"/templates/styles/%s.css"%(fname)).read()), setCookies=None, setContentType="text/css")
