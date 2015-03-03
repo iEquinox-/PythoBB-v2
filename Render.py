c007f17eb480b4380b7b16a8c9dcad71fb4c99ae
Grayson Equinox <duubz@skid.pro>
2015-03-02 19:17:21 -0600

    Update Render.py

--- Render.py
+++ Render.py
@@ -1,9 +1,11 @@
 import json,types
+from django.http import HttpResponse
 
 class Render():
 	def __init__(self):
 		self._JSON    = self.RenderJSON
 		self.JSONDict = dict()
+		self._Page    = self.RenderPage
 		
 	def RenderJSON(self, variable=None, boolean=None, data=None, complete=False, get=None):
 		if(isinstance(get, types.NoneType)):
@@ -12,3 +14,17 @@ class Render():
 				return self.JSONDict
 		else:
 			return self.JSONDict[get]
+
+	def RenderPage(self, content=None, setCookies=None):
+		_RenderedContent = HttpResponse(content) if not isinstance(content, types.NoneType) else HttpResponse(self.RenderError(Error=1))
+		if(isinstance(setCookies, types.DictType))and(len(setCookies)>=1):
+			for c in setCookies:
+				_RenderedContent.set_cookie(str(c), setCookies[c])
+		return _RenderedContent
+		
+	def RenderError(self, Error=None):
+		keys = {
+			1: "Missing or unloaded content: Render()._Page function."
+			}
+		if isinstance(Error, types.IntType):
+			return keys[Error]
