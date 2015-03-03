868dde51abcaac00dba2281141891c6feaad60d6
Grayson Equinox <duubz@skid.pro>
2015-03-02 20:07:48 -0600

    Create Pages.py

---
+++ Pages.py
@@ -0,0 +1,12 @@
+
+class Pages():
+	def __init__(self):
+		self._Render = OpenPage
+		self.pageKeys = {
+			"index": "index",
+			"userblock_guest": "",
+			"userblock_user": ""
+			}
+		
+	def OpenPage(self, name=None):
+		return str(open("templates/%s.ptmp"%(name)).read())
