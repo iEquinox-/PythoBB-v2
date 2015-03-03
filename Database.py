a6027f792c36e7388a710dc62a8dbe5253df3b73
Grayson Equinox <duubz@skid.pro>
2015-03-03 02:18:42 -0600

    Update Database.py

--- Database.py
+++ Database.py
@@ -2,23 +2,30 @@ import sqlite3,types,Security,Settings
 
 class Database():
 	def __init__(self):
-		self._DB = self.Parse_DB(db_dir=Settings.BASEDIR + "pythobb.db")
+		self._DB = self.Parse_DB(db_dir=Settings.BASEDIR + "/pythobb.db")
 		self.exe = self.Execute
 		
 	def Parse_DB(self, db_dir=None):
 		if not db_dir:
 			db_dir = Settings.BASEDIR + "pythobb.db"
 		connection = sqlite3.connect(db_dir)
-		cursor     = c.cursor()
+		cursor     = connection.cursor()
 		execute    = cursor.execute
 		return {"connection":connection, "cursor":cursor, "execute":execute}
 		
-	def Execute(self, query=None, variables=None, commit=False, doReturn=False):
-		if( not isinstance(query, types.NoneType) )and( isinstance(variables, types.TupleType) ):
-			c = self._DB["execute"](query, variables)
-			if commit == True:
-				self._DB["connection"].commit()
-			if doReturn == True:
-				return [x for x in c]
+	def Execute(self, query=None, variables=None, commit=False, doReturn=False, string=False):
+		if string:
+			if( not isinstance(query, types.NoneType) )and( isinstance(variables, types.TupleType) ):
+				try:
+					c = self._DB["execute"](query, variables)
+					if commit == True:
+						self._DB["connection"].commit()
+					if doReturn == True:
+						return [x for x in c]
+				except Exception as e:
+					return [None, "Error parsed " + str(e)]
+			else:
+				return [None, "Error, missing or obstructed data. [QUERY] " + query + " [VARIABLES] " + str(variables)]
 		else:
-			return [None, "Error, missing or obstructed data."]
+			return
+
