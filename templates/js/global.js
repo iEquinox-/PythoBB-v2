943d76e4110ef2077a83afb512d5eca70ccb1e09
Grayson Equinox <duubz@skid.pro>
2015-03-03 20:41:30 -0600

    Update global.js

--- templates/js/global.js
+++ templates/js/global.js
@@ -1,25 +1,76 @@
 jQuery(document).ready(function($){
-	var id = 1;
+	var id = 1,
+		cookieArray = getCookie("catClosed").split(",");
+	/* Cookie function: https://docs.djangoproject.com/en/dev/ref/csrf/ */
+	function getCookie(name) {
+		var cookieValue = null;
+		if( document.cookie && document.cookie != '' ) {
+			var cookies = document.cookie.split(';');
+			for (var i = 0; i < cookies.length; i++ ) {
+				var cookie = jQuery.trim(cookies[i]);
+				if (cookie.substring(0,name.length+1)==(name+'=')) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length+1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
+	/* End Cookie function */
 	function SendAlert(string) {
-		$("body").append("<div id=\"alert\" class=\"alertid-" + id + "\"><div class=\"part-left\">!</div><div class=\"part-right\">" + string + "</div></div>");
-		var alert = $("div#alert.alertid-"+id);
-		alert.fadeIn(500); id += 1;
-		setTimeout(function(){
-			alert.fadeOut(500);
+		var alert_content = "<div class=\"part-left\">!</div><div class=\"part-right\">" + string + "</div></div>";
+		if( getCookie("allowAlerts") == "True" ) {
+			if( $("div#alert").length ) {
+				if( $("div#alert").length < 5 ) {
+					$("body").append("<div id=\"alert\" class=\"alertid-" + id + "\" style=\"margin-bottom:" + 50 * $("div#alert").length + "px;\">" + alert_content);
+				}
+			} else {
+				$("body").append("<div id=\"alert\" class=\"alertid-" + id + "\">" + alert_content);
+			}
+			var alert = $("div#alert.alertid-"+id);
+			alert.fadeIn(500); id += 1;
 			setTimeout(function(){
-				alert.remove()
-			},500);
-			id -= 1;
-		}, 5000);
+				alert.fadeOut(500);
+				setTimeout(function(){
+					alert.remove()
+				},500);
+				id -= 1;
+			}, 5000);
+		}
 	}
-	function TriggerCat(cid) {
+	function TriggerCat(cid, full) {
 		var cat = $("div#cat.catid-"+cid);
-		cat.children(".cbody").fadeToggle(500);
-		cat.children(".cfoot").fadeToggle(500);
-		setTimeout(function(){ cat.toggleClass("closed"); }, 500);
+		if(full == false){
+			cat.children(".cbody").fadeToggle(500); cat.children(".cfoot").fadeToggle(500);
+			setTimeout(function(){
+				cat.toggleClass("closed");
+				if( cat.hasClass("closed") ){
+						cookieArray.push(cid);
+						document.cookie = ("catClosed=" + cookieArray.join() + ";")
+				} else {
+						cookieArray.splice(cookieArray.indexOf(cid),1)
+						document.cookie = ("catClosed=" + cookieArray.join() + ";")
+				}
+			}, 500);
+		} else {
+			cat.toggleClass("closed");
+			if( cat.hasClass("closed") ){
+					cookieArray.push(cid);
+					document.cookie = ("catClosed=" + cookieArray.join() + ";")
+			} else {
+					cookieArray.splice(cookieArray.indexOf(cid),1)
+					document.cookie = ("catClosed=" + cookieArray.join() + ";")
+			}
+		}
 	}
+	
+	for(var c in cookieArray){
+		$("div#cat.catid-"+c).addClass("closed")
+	}	
+	
 	$("div#cat>div.cname>a.togglecat").on("click", function(){
 		cid = $(this).parent().parent().attr('class').substring(6).split(" ")[0]
-		TriggerCat(cid);
+		TriggerCat(cid, false);
+		SendAlert('Category toggled.')
 	});
 });
