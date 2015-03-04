66fe7d151094d68579c69b7259c8212ba1042b0f
Grayson Equinox <duubz@skid.pro>
2015-03-03 19:25:16 -0600

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+import Frame,Pages
+
+urlpatterns = patterns('',
+  url(r'^$', Frame.Base().Home, name='Index'),
+	url(r'^js/(?P<fname>\w+).js', Pages.Pages()._JS, name='JavaScript File'),
+	url(r'^css/(?P<fname>\w+).css$', Pages.Pages()._CSS, name='CSS File'),
+)
