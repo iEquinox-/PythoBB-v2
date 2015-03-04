d4cb80791102d3582175d78a5c07a247fdd7df5f
Grayson Equinox <duubz@skid.pro>
2015-03-04 00:03:58 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ import Frame,Pages
 
 urlpatterns = patterns('',
 	url(r'^$', Frame.Base().Home, name='Index'),
+	url(r'^login/$', Frame.Base().Login, name='Login'),
 	url(r'^js/(?P<fname>\w+).js', Pages.Pages()._JS, name='JavaScript File'),
 	url(r'^css/(?P<fname>\w+).css$', Pages.Pages()._CSS, name='CSS File'),
 )
