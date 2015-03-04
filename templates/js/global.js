b4cb79f9dac5d133b88891af86d38dbc62e4d82f
Grayson Equinox <duubz@skid.pro>
2015-03-03 18:42:38 -0600

    Update global.js

--- templates/js/global.js
+++ templates/js/global.js
@@ -1,1 +1,15 @@
-alert('test');
+jQuery(document).ready(function($){
+	var id = 1;
+	function SendAlert(string) {
+		$("body").append("<div id=\"alert\" class=\"alertid-" + id + "\"><div class=\"part-left\">!</div><div class=\"part-right\">" + string + "</div></div>");
+		var alert = $("div#alert.alertid-"+id);
+		alert.fadeIn(500); id += 1;
+		setTimeout(function(){
+			alert.fadeOut(500);
+			setTimeout(function(){
+				alert.remove()
+			},500);
+			id -= 1;
+		}, 5000);
+	}
+});
