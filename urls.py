38da50f51e73baf6ed43c70197794bdce94e50c3
Grayson Equinox <duubz@skid.pro>
2015-03-03 19:25:37 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 import Frame,Pages
 
 urlpatterns = patterns('',
-  url(r'^$', Frame.Base().Home, name='Index'),
+	url(r'^$', Frame.Base().Home, name='Index'),
 	url(r'^js/(?P<fname>\w+).js', Pages.Pages()._JS, name='JavaScript File'),
 	url(r'^css/(?P<fname>\w+).css$', Pages.Pages()._CSS, name='CSS File'),
 )
