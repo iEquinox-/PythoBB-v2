bf81bec7c48aed0710c0d7dd3bab9c7d27c770b2
Grayson Equinox <duubz@skid.pro>
2015-03-05 13:59:59 -0600

    Create IDs.py

---
+++ IDs.py
@@ -0,0 +1,16 @@
+import Database,types
+
+class Values():
+	def __init__(self):
+		self._UID = self.getNewID(type="UID")
+		self._CID = self.getNewID(type="CID")
+		
+	def getNewID(self, type=None):
+		if not isinstance(type, types.NoneType):
+			typeTables = {
+				"UID": "users",
+				"CID": "categories",
+				"FID": "forums"
+			}
+			value = len(Database.Database().Execute(query="SELECT * FROM pythobb_%s"%(typeTables[type]), variables=(), commit=False, doReturn=True))
+			return int(value+1)
