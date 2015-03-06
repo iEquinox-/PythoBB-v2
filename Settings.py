47b0357f1c7307de222db561df647153d19a384d
Grayson Equinox <duubz@skid.pro>
2015-03-05 20:01:46 -0600

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,14 +1,16 @@
 import os
 
 FORUMURL    = "http://127.0.0.1:8000"
-FORUMNAME   = "PythoBB"
+FORUMNAME   = "PythoBB v2"
 TABLEPREFIX = "pythobb_"
-BASEDIR     = os.path.dirname(os.path.abspath(__file__))
 
+# DON'T EDIT THE FOLLOWING SETTINGS
+
+BASEDIR     = os.path.dirname(os.path.abspath(__file__))
 TABLESCHEMA = """
 			CREATE TABLE pythobb_categories (cid int, name text, desc text)
 			CREATE TABLE pythobb_forums (fid int, parent int, name text, desc text)
-			
 			CREATE TABLE pythobb_users (uid int, username text, salt text, password text)
 			CREATE TABLE pythobb_user_data (uid int, sessionid text, ipaddr text)
+			CREATE TABLE pythobb_user_data2 (uid int, email text, avatar text, usertitle text, gid int)
 			"""
