97e699a941c0e681c3175daf710b9831da1f20e2
Grayson Equinox <duubz@skid.pro>
2015-03-07 14:28:56 -0600

    Create Groups.py

---
+++ Groups.py
@@ -0,0 +1,11 @@
+
+# Groups module for PythoBB
+# 1 must be Administrator, and 2 must be Default user.
+# Do NOT edit group 1, if you do it may reassign the two
+# values, and users who are in the second user group
+# (default) will have administrator permissions.
+
+Groups = {
+		1: {"Name": "Administrator", "Description":"Site administrator."}
+		2: {"Name": "Default user", "Description":"Forum member"}
+	}
