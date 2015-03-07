e70f5bc01407f550de9b02eebb305bee3634feb7
Grayson Equinox <duubz@skid.pro>
2015-03-07 12:37:44 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-import Frame,Pages
+import Frame,Pages,Settings,API
 
 urlpatterns = patterns('',
 	url(r'^$', Frame.Base().Home, name='Index'),
 	url(r'^login/$', Frame.Base().Login, name='Login'),
 	url(r'^login/_login/$', Frame.Base().ProcessLogin, name='Login'),
+	url(r'^register/$', Frame.Base().Register, name='Register'),
+	url(r'^register/_register/$', Frame.Base().ProcessRegister, name='Register'),
+	url(r'^member/profile/(?P<uid>\d+)/$', Frame.Base().MakeProfile, name='Profile'),
+	#url(r'^member/usercp/$', Frame.Base().ControlPanel, name='User CP'),
 	url(r'^js/(?P<fname>\w+).js', Pages.Pages()._JS, name='JavaScript File'),
 	url(r'^css/(?P<fname>\w+).css$', Pages.Pages()._CSS, name='CSS File'),
+	url(r'^api/v1/(?P<type>\w+)/(?P<requested>\d+|\*)/$', API.API().RenderJSON, name='API')
 )
