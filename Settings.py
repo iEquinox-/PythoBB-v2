98108d91bbfe7e9ed3f50411c137fe0c768ceb38
Grayson Equinox <duubz@skid.pro>
2015-03-08 17:01:15 -0500

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -11,6 +11,8 @@ BASEDIR     = os.path.dirname(os.path.abspath(__file__))
 TABLESCHEMA = """
 			CREATE TABLE pythobb_categories (cid int, name text, desc text)
 			CREATE TABLE pythobb_forums (fid int, parent int, name text, desc text)
+			CREATE TABLE pythobb_threads (tid int, parent int, name text, tags text)
+			CREATE TABLE pythobb_posts (pid int, parent int, content text, uid int)
 			CREATE TABLE pythobb_users (uid int, username text, salt text, password text)
 			CREATE TABLE pythobb_user_data (uid int, sessionid text, ipaddr text)
 			CREATE TABLE pythobb_user_data2 (uid int, email text, avatar text, usertitle text, gid int)
